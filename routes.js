805892a506818c3f7b37086e43786c397b6f7138
koyeary <katyeary@gmail.com>
2021-08-30 15:11:36 -0700

    render gallery page

--- routes.js
+++ routes.js
@@ -3,9 +3,11 @@ module.exports = app => {
     const application = require('./routes/application');
     const failure = require('./routes/failure');
     const mail = require('./routes/transporter');
+    const gallery = require('./routes/gallery');
 
     app.use('/', application);
     app.use('/failure', failure);
     app.use('/mail', mail);
+    app.use('/gallery', gallery)
 
 }
