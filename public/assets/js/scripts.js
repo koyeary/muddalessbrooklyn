c1cb8943cb92f2c312dcefceaa9c0d4347f48054
koyeary <katyeary@gmail.com>
2021-09-03 12:07:14 -0700

    lots of little style tweaking. modal opens lightbox onclick

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,3 +1,25 @@
+//Stick navbar
+const height = $(window).height();
+
+$(window).on('scroll', () => {
+  if (window.scrollY > height - 50) {
+    $('.navbar').addClass('fixed-top shadow');
+    $('.navbar').removeClass('mt-n4');
+    $('.links').addClass('my-3');
+    $('span').removeClass('d-none py-2');
+    $('a').removeClass('main-nav btn pb-2 pt-2 px-4');
+    $('ul').addClass('secondary-nav');
+    
+  } else {
+    $('.navbar').removeClass('fixed-top shadow');
+    $('.navbar').addClass('mt-n4');
+    $('.links').removeClass('my-3');
+    $('span').addClass('d-none');
+    $('a').addClass('main-nav btn pb-2 pt-2 px-4');
+    $('ul').removeClass('secondary-nav');
+  }
+})
+
 //Nodemailer request
 
 const postReq = (name, email, phone, subject, message) => {
@@ -26,21 +48,48 @@ $('#mail').on('click', async (e) => {
 
     await postReq(name, email, phone, subject, message);
 
-    $('#contactAlert').modal(console.log('sent'));
+    $('#contactAlert').modal('show');
   } catch (err) {
     console.error(err);
   }
 });
 
-//Load image gallery
+// Modal Image Gallery
+/* const modal = $('#myModal');
 
-//$('.lightbox').modal(console.log('zoom'));
-/* $('.lightbox').on('show.bs.modal', (e) => {
-  const button = $(e.relatedTarget);
-  const image = button.data('whatever');
-  const modal = $(this);
+const modalImg = $('#img01');
+*/
 
-  modal.find('gallery-image').attr('src', image);
-}); */
-//clear form on modal close
-//$('#messageSent').on('click', location.reload());
+$('.lightbox').on('click', () => {
+  $('#exampleModal').modal('show');
+console.log('at least i can click it');
+});
+
+
+ 
+
+// Close the modal
+/* const span = $('.close');
+span.on('click', () => {
+  modal.style.display = 'none';
+})
+ */
+/*var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+} */
