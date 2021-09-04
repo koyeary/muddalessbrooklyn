a633745ee37a4579f3d4e7f34ff51367e3fd3fe6
koyeary <katyeary@gmail.com>
2021-09-03 19:27:40 -0700

    add product build

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express   = require('express');
+const express = require('express');
 const connectDB = require('./config/db');
-const path      = require('path');
-const logger    = require('morgan');
-const exphbs    = require('express-handlebars');
-const routes    = require('./routes');
+const path = require('path');
+const logger = require('morgan');
+const exphbs = require('express-handlebars');
+const routes = require('./routes');
 
-const app       = express();
+const app = express();
 
 // Connect Database
 connectDB();
@@ -21,25 +21,38 @@ app.use(routes);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  //
+  app.get('*', (req, res) => {
+    res.send(path.join((__dirname = 'client/build/index.html')));
+  });
+}
+//build mode
+app.get('*', (req, res) => {
+  res.send(path.join(__dirname + '/client/public/index.html'));
+});
 
 //Set up handlebars
-app.engine('handlebars', exphbs({
+app.engine(
+  'handlebars',
+  exphbs({
     defaultLayout: 'main'
-}));
+  })
+);
 app.set('view engine', 'handlebars');
-
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
-  // catch 404 and forward to error handler
+// catch 404 and forward to error handler
 /* app.use((req, res, next) => {
     const err = new Error('Not Found');
     err.status = 404;
     next(err);
   }); */
-  
+
 // error handler
 // no stacktraces leaked to user unless in development environment
 /* app.use((err, req, res, next) => {
