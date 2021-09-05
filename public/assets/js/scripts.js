36dff31a2a7a82057972a4bec646619e073fbc8b
koyeary <katyeary@gmail.com>
2021-09-04 19:56:20 -0700

    replace deleted parser

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -9,7 +9,6 @@ $(window).on('scroll', () => {
     $('span').removeClass('d-none py-2');
     $('a').removeClass('main-nav btn pb-2 pt-2 px-4');
     $('ul').addClass('secondary-nav');
-    
   } else {
     $('.navbar').removeClass('fixed-top shadow');
     $('.navbar').addClass('mt-n4');
@@ -18,12 +17,12 @@ $(window).on('scroll', () => {
     $('a').addClass('main-nav btn pb-2 pt-2 px-4');
     $('ul').removeClass('secondary-nav');
   }
-})
+});
 
 //Nodemailer request
-
 const postReq = (name, email, phone, subject, message) => {
-  $.ajax('/mail', {
+
+   $.ajax('/api/mail', {
     method: 'POST',
     data: {
       name: name,
@@ -31,33 +30,52 @@ const postReq = (name, email, phone, subject, message) => {
       phone: phone,
       subject: subject,
       message: message
+    },
+    success: (res) => {
+      //$('#contactAlert').modal(console.log(res));
+      alert(res.data);
     }
-  });
+  }); 
 };
 
-// Send the POST request.
-$('#mail').on('click', async (e) => {
+
+$('#mailSubmit').on('click', (e) => {
   e.preventDefault();
 
-  try {
-    const name = $('#name-input').val().trim();
-    const email = $('#email-input').val().trim();
-    const phone = $('#phone-input').val().trim();
-    const subject = $('#subject-input').val().trim();
-    const message = $('#message-input').val().trim();
+  const name = $('#name-input').val().trim();
+  const email = $('#email-input').val().trim();
+  const phone = $('#phone-input').val().trim();
+  const subject = $('#subject-input').val().trim();
+  const message = $('#message-input').val().trim();
 
-    await postReq(name, email, phone, subject, message);
-
-    $('#contactAlert').modal('show');
-  } catch (err) {
-    console.error(err);
-  }
+  postReq(name, email, phone, subject, message);
+  alert('message sent');
+  location.reload();
+  
 });
+
+// Send the POST request.
+//$('#mail').on('submit', (e) => {
+
+//e.preventDefault();
+
+//postReq(myName, email, phone, subject, message);
+
+//await console.log(`${name} ${email} ${phone} ${subject} ${message}`);
+//await postReq();
+//$('#contactAlert').show();
+
+//});
+/* $('#messageSent').on('click', () => {
+  location.reload();
+}); */
+
+//
 
 // Modal Image Gallery
 
- $('.lightbox').on('click', (e) => {
-   e.preventDefault();
+$('.lightbox').on('click', (e) => {
+  e.preventDefault();
 
   const thisId = e.currentTarget.id;
   $(`#my${thisId}`).modal('show');
@@ -66,5 +84,4 @@ $('#mail').on('click', async (e) => {
 $('.btn-close').on('click', () => {
   const myId = $(this).attr('id');
   $(`#my${myId}`).modal('hide');
-}); 
- 
+});
