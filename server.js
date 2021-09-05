b41808a4f26bc3a55b5e0146938d66936340c3b3
koyeary <katyeary@gmail.com>
2021-09-05 14:02:33 -0700

    serve no favicon

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ const exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+app.get('/favicon.ico', (req, res) => res.status(200));
 
 
 //Listener
