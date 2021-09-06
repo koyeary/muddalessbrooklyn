8f999485fa65a8134ccc2dbd7472e269fed673de
koyeary <katyeary@gmail.com>
2021-09-06 14:19:24 -0700

    passport config - userprifleurl still needs to be added

---
+++ config/passport.js
@@ -0,0 +1,18 @@
+const auth = require('./auth');
+
+const GoogleOauthStrategy = require('passport-google-oauth20').Strategy;
+
+module.exports = (passport) => {
+
+    passport.serializeUser((user, done) => done(null, user));
+    passport.deserializeUser((user, done) => done(null, user));
+    passport.use(new GoogleOauthStrategy(
+        {
+          clientID: auth.client,
+          clientSecret: auth.secret,
+          callbackURL: auth.callback,
+          // Set the correct profile URL that does not require any additional APIs
+          userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo'
+        },
+        (token, refreshToken, profile, done) => done(null, {profile, token})));
+}
