d834e01838d0683bf793bba5794d40f0d875ebf7
koyeary <katyeary@gmail.com>
2021-09-14 17:34:11 -0700

    typography and about me adjustments

--- routes/api/images.js
+++ routes/api/images.js
@@ -11,39 +11,4 @@ const Image     = require('../../models/Image');
   }
 });
 
-/*
-router.put('/:id', async (req, res) => {
-  const { id, rating } = req.body;
-
-  try {
-    const image = await Image.updateOne({ id, rating });
-
-    if (!image) {
-      return res.status(400).json({ msg: 'Image not found' });
-    }
-
-    await image.save();
-    res.status(200).json({ msg: 'Image updated' });
-  } catch (err) {
-    return console.error(err);
-  }
-}); */
-
-/* router.post('/', async (req, res) => {
-    const { image, location, hero } = req.body;
-    try {
-        const img = new Image({
-            image,
-            location,
-            hero
-        });
-
-        await img.save();
-        return res.status(200).json({ msg: 'image successfully created'});
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server error');
-    }
-}); */
-
 module.exports = router;
--- routes/api/transporter.js
+++ routes/api/transporter.js
@@ -7,77 +7,30 @@ const pass = process.env.MAIL_PASS || config.get('MAIL_PASS');
 
 // POST route from contact form
 router.post('/', async (req, res, next) => {
-
-  try {
-
-    const transporter = nodemailer.createTransport({
-      service: 'yahoo',
-      auth: {
-        user: user,
-        pass: pass
-      }
-    });
-
-    const mailOpts = {
-      from: user, // This is ignored by Gmail
-      to: 'katyeary@gmail.com',
-      subject: `${req.body.subject}`,
-      text: `${req.body.sender} (${req.body.email}) says: ${req.body.message} call ${req.body.phone}`
-    };
-
-    transporter.sendMail(mailOpts, (error, res) => {
-      if (error) {
-        console.log(error);
-        console.log(`Failed contact form attempt: ${mailOpts}`);
-        //router.get('/failure');
-      } else {
-        return console.log('Contact-form attempt successful');
-      }
-    });
-  } catch (err) {
-    return console.error(err);
-  }
-});
-//const { sender, email, message, phone, subject } =req.body;
-
-/*   let transporter = nodemailer.createTransport({
+  const transporter = nodemailer.createTransport({
     service: 'yahoo',
     auth: {
       user: user,
       pass: pass
     }
-  }); */
+  });
 
-// Specify what the email will look like
-/*  const mailOpts = {
-    from: user, // This is ignored by Gmail
+  const mailOpts = {
+    from: user,
     to: 'katyeary@gmail.com',
     subject: `${req.body.subject}`,
     text: `${req.body.sender} (${req.body.email}) says: ${req.body.message} call ${req.body.phone}`
   };
 
-  // Attempt to send the email
   transporter.sendMail(mailOpts, (error, res) => {
     if (error) {
-      console.log(error);
+      console.error(error);
       console.log(`Failed contact form attempt: ${mailOpts}`);
-      //router.get('/failure');
     } else {
-      //res.render('contact-success')
       console.log('Contact-form attempt successful');
     }
-    console.log(res.response);
-  }); */
-
-// Debug / verification
-/*    transporter.verify(function (error, success) {
-    if (error) {
-      console.log('not connecting to smtp');
-    } else {
-      console.log('Server ready to take messages');
-      console.log(mailOpts);
-    }
-  });  */
-//});
+  });
+  next(res.json({ msg: 'Your message has been sent'}));
+});
 
 module.exports = router;
