0c918fd08946a23b9adb4a1da1418e6853f3c871
koyeary <katyeary@gmail.com>
2021-09-22 19:34:00 -0700

    sort images on rating

--- routes/html/gallery.js
+++ routes/html/gallery.js
@@ -2,31 +2,32 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-   try {
-    
+  try {
     const images = await Image.find({});
+    const sortedImages = [...images].sort((a, b) => {
+      return a.rating - b.rating;
+    });
 
-     const hbsObject =  {
+    const hbsObject = {
       layout: 'gallery',
-      galleryDocument: images.map((doc) => {
-          return {
-            _id: doc._id,
-            location: doc.location,
-            galleryImage: doc.image,
-            hero: doc.hero,
-            rating: doc.rating,
-            notes: doc.notes
-          };
+
+      galleryDocument: sortedImages.map((doc) => {
+        return {
+          _id: doc._id,
+          location: doc.location,
+          galleryImage: doc.image,
+          hero: doc.hero,
+          rating: doc.rating,
+          notes: doc.notes
+        };
       })
-    }
+    };
 
     console.log(hbsObject);
-    return res.render('gallery', hbsObject ); 
- 
+    return res.render('gallery', hbsObject);
   } catch (err) {
     return console.error(err);
-  } 
+  }
 });
-
 
 module.exports = router;
