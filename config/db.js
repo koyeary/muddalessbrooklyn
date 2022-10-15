1baa542251ac08d3af2313561ce85aa85139911f
Kat Yeary <kat@ScholartsMBPKat.lan>
2022-10-15 14:03:09 -0700

    add usenewurlparser

--- config/db.js
+++ config/db.js
@@ -6,7 +6,6 @@ const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
 
     console.log("MongoDB Connected...");
