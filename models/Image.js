4739af58da2e4c1772f8cec30976182b6d3d9f3f
Kat Yeary <katyeary@gmail.com>
2023-02-04 17:28:01 -0500

    update mongoose strictQuery

--- models/Image.js
+++ models/Image.js
@@ -1,40 +1,41 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
+mongoose.set("strictQuery", false);
 var ImageSchema = new Schema({
-  image: { 
+  image: {
     type: String,
-    required: true },
-  location: { 
-    type: String, 
-    required: true 
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
   },
   hero: {
     type: Boolean,
-    default: false
+    default: false,
   },
   rating: {
-    type: Number
+    type: Number,
   },
   notes: {
-    type: String
+    type: String,
   },
   created_at: Date,
-  updated_at: Date
+  updated_at: Date,
 });
 
 /**
-* Add the dates on save ( updated_at & created_at )
-*/
-ImageSchema.pre('save', next => {
+ * Add the dates on save ( updated_at & created_at )
+ */
+ImageSchema.pre("save", (next) => {
   let currentDate = new Date();
 
   this.updated_at = currentDate;
 
-  if(!this.created_at) this.created_at = currentDate;
+  if (!this.created_at) this.created_at = currentDate;
 
   next();
 });
 
-module.exports = Image = mongoose.model('image', ImageSchema);
-
+module.exports = Image = mongoose.model("image", ImageSchema);
