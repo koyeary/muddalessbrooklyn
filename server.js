23dd28a08c471d8acbb58947969006e615ec0394
Kat Yeary <katyeary@gmail.com>
2023-02-04 14:57:35 -0500

    netlify fix

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const express = require("express");
 const favicon = require("serve-favicon");
 const connectDB = require("./config/db");
 const path = require("path");
+const serverless = require("serverless-http");
 const logger = require("morgan");
 const routes = require("./routes");
 
 const app = express();
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3001;
 
 // Connect Database
@@ -15,6 +17,7 @@ connectDB();
 
 //Middleware
 app.use(logger("dev"));
+app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false, limit: "20mb" }));
 
@@ -35,7 +38,7 @@ app.use(favicon(__dirname + "/public/favicon.ico"));
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use("/.netlify/functions/server", express.Router);
 //Routes
 app.use(routes);
 
@@ -43,3 +46,6 @@ app.use(routes);
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
+
+module.exports = app;
+module.exports.handler = serverless(app);
